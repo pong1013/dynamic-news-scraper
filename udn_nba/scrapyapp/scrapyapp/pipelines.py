f384bba34722389f74182af73d13c4a5fe322116
pong861013 <pong861013@gmail.com>
2024-05-14 09:19:41 +0000

    fix static and pipeline bug

--- udn_nba/scrapyapp/scrapyapp/pipelines.py
+++ udn_nba/scrapyapp/scrapyapp/pipelines.py
@@ -9,6 +9,7 @@
 from warehouse.models import UdnFocus
 from asgiref.sync import sync_to_async
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 
 class ScrapyappPipeline:
     async def process_item(self, item, spider):
@@ -20,18 +21,19 @@ class ScrapyappPipeline:
             pass
         
             
-        news = UdnFocus()
-        news.title = item['title']
-        news.author = item['author']
-        news.publish_time = item['publish_time']
-        news.content = item['content']
-        await sync_to_async(news.save)()
-        
-        # Check if total records exceed 10
-        if await sync_to_async(UdnFocus.objects.count)() > 10:
-            # Delete the oldest record
-            oldest_news = await sync_to_async(UdnFocus.objects.order_by('publish_time').first)()
-            await sync_to_async(oldest_news.delete)()
+        async with transaction.atomic():
+            news = UdnFocus()
+            news.title = item['title']
+            news.author = item['author']
+            news.publish_time = item['publish_time']
+            news.content = item['content']
+            await sync_to_async(news.save)()
+            
+            # Check if total records exceed 10
+            if await sync_to_async(UdnFocus.objects.count)() > 10:
+                # Delete the oldest record
+                oldest_news = await sync_to_async(UdnFocus.objects.order_by('publish_time').first)()
+                await sync_to_async(oldest_news.delete)()
         
             
         return item
